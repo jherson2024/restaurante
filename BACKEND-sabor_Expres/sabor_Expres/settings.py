0f20bd54d2c1ed3c73371276c5bee136dc141db6
Jherson <jinca@unsa.edu.pe>
2024-07-13 10:45:59 -0500

    configurando admin en django

--- BACKEND-sabor_Expres/sabor_Expres/settings.py
+++ BACKEND-sabor_Expres/sabor_Expres/settings.py
@@ -87,12 +87,8 @@ WSGI_APPLICATION = 'sabor_Expres.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'res',
-        'USER': 'root',
-        'PASSWORD': '1234',
-        'HOST': 'localhost',  # O la dirección IP de tu servidor MySQL
-        'PORT': '3306', 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
 
