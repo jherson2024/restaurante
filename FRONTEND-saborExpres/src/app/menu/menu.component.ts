5a34125128b8eef8de76fcdc252a0b13264fb565
Jherson <jinca@unsa.edu.pe>
2024-07-18 21:10:03 -0500

    animación al agregar un producto al carrito

--- FRONTEND-saborExpres/src/app/menu/menu.component.ts
+++ FRONTEND-saborExpres/src/app/menu/menu.component.ts
@@ -1,7 +1,7 @@
 // menu.component.ts
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { ApiService } from '../api.service';
@@ -28,8 +28,9 @@ export class MenuComponent implements OnInit {
   productos: Producto[] = [];
   carrito: { producto_id: number, cliente_id: number }[] = [];
   clienteId = 1;  // ID del cliente
+  carritoCount = 0; 
 
-  constructor(private apiService: ApiService, private authService: AuthService,private renderer: Renderer2) {
+  constructor(private apiService: ApiService, private authService: AuthService,private renderer: Renderer2, private el: ElementRef) {
     this.clienteId = this.authService.getClienteId();
     if (this.clienteId === 0) {
       this.clienteId = 4;  // Default value if not set
@@ -38,6 +39,7 @@ export class MenuComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadProductos();
+    this.obrenerNumeroProductosCarrito();
   }
 
   loadProductos(): void {
@@ -54,6 +56,7 @@ export class MenuComponent implements OnInit {
   showProductId(productId: number, clienteId: number): void {
     this.apiService.agregarProductoAlCarrito(clienteId, productId).subscribe(
       response => {
+        this.carritoCount++; 
       },
       error => {
         console.error('Error adding product to cart:', error);
@@ -62,11 +65,43 @@ export class MenuComponent implements OnInit {
   }
   moveToCart(event: any, productId: number, clienteId: number): void {
     const imageElement = event.target as HTMLElement;
-    this.renderer.addClass(imageElement, 'moving-to-cart');
-    
-    imageElement.addEventListener('animationend', () => {
+    const clonedImage = imageElement.cloneNode(true) as HTMLElement;
+    document.body.appendChild(clonedImage);
+
+    const cartIcon = this.el.nativeElement.querySelector('#cart-icon');
+    const cartRect = cartIcon.getBoundingClientRect();
+    const imgRect = imageElement.getBoundingClientRect();
+
+    const deltaX = cartRect.left - imgRect.left;
+    const deltaY = cartRect.top - imgRect.top;
+
+    this.renderer.setStyle(clonedImage, 'position', 'absolute');
+    this.renderer.setStyle(clonedImage, 'top', `${imgRect.top}px`);
+    this.renderer.setStyle(clonedImage, 'left', `${imgRect.left}px`);
+    this.renderer.setStyle(clonedImage, 'width', `${imgRect.width}px`);
+    this.renderer.setStyle(clonedImage, 'height', `${imgRect.height}px`);
+    this.renderer.setStyle(clonedImage, 'transition', 'transform 1s ease-in-out, opacity 1s');
+    this.renderer.setStyle(clonedImage, 'transform', `translate(${deltaX}px, ${deltaY}px)`);
+    this.renderer.setStyle(clonedImage, 'opacity', '0.5');
+
+    clonedImage.addEventListener('transitionend', () => {
       this.showProductId(productId, clienteId);
-      this.renderer.removeClass(imageElement, 'moving-to-cart');
+      document.body.removeChild(clonedImage);
     }, { once: true });
+  }
+  obrenerNumeroProductosCarrito(): void {
+    this.apiService.getProductosCarrito(this.clienteId).subscribe(
+      data => {
+        let totalProductos = 0;
+        if (Array.isArray(data) && data.length > 0) {
+          totalProductos = data.reduce((total, item) => total + (item.cantidad || 1), 0);
+        }
+        this.carritoCount = totalProductos; // Actualiza el contador del carrito
+        console.log('Número de productos en el carrito:', this.carritoCount);
+      },
+      error => {
+        console.error('Error fetching cart products:', error);
+      }
+    );
   }
 }
