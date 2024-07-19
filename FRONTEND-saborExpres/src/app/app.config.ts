33f5f76323dc57433ab8171786bce54a59cd5cbb
Fiorela <clafiorelariza@gmail.com>
2024-07-19 09:23:00 -0500

    cambiando las direcciones de ruta a app.routes.ts

--- FRONTEND-saborExpres/src/app/app.config.ts
+++ FRONTEND-saborExpres/src/app/app.config.ts
@@ -1,30 +1,9 @@
-import { ApplicationConfig} from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { MenuComponent } from './menu/menu.component';
-import { RegisterComponent } from './register/register.component';
-import { LogoutComponent } from './logout/logout.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
-import { UserLoginComponent } from './user-login/user-login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { ContactanosComponent } from './contactanos/contactanos.component';
+
+import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter([
-      {path: 'contactanos', component: ContactanosComponent},
-  {path: 'login', component: LoginComponent},
-      {path: 'menu', component: MenuComponent},
-      {path: 'register', component: RegisterComponent},
-      {path: 'logout', component: LogoutComponent},
-      {path: 'navBar', component: NavBarComponent},
-      { path: 'user-login', component: UserLoginComponent },
-  { path: 'dashboard', component: DashboardComponent },
-      { path: 'cart', loadComponent: () => import('./cart/cart.component').then(m => m.CartComponent) },
-  { path: 'pago', loadComponent: () => import('./pago/pago.component').then(m => m.PagoComponent) },
-  { path: 'confirmacion-orden', loadComponent: () => import('./confirmacion-orden/confirmacion-orden.component').then(m => m.ConfirmacionOrdenComponent) }
- ]),
-  ],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
 };
-
-
--- FRONTEND-saborExpres/src/app/app.routes.ts
+++ FRONTEND-saborExpres/src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { ContactanosComponent } from './contactanos/contactanos.component';
+import { LoginComponent } from './login/login.component';
+import { MenuComponent } from './menu/menu.component';
+import { RegisterComponent } from './register/register.component';
+import { LogoutComponent } from './logout/logout.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: 'contactanos', component: ContactanosComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'menu', component: MenuComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'logout', component: LogoutComponent },
+    { path: 'navBar', component: NavBarComponent },
+    { path: 'user-login', component: UserLoginComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'cart',
+      loadComponent: () =>
+        import('./cart/cart.component').then((m) => m.CartComponent),
+    },
+    { path: 'pago',
+      loadComponent: () =>
+        import('./pago/pago.component').then((m) => m.PagoComponent),
+    },
+    { path: 'confirmacion-orden',
+      loadComponent: () =>
+        import('./confirmacion-orden/confirmacion-orden.component').then(
+          (m) => m.ConfirmacionOrdenComponent
+        ),
+    },
+
+];
